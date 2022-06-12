75a9e3f122afd8e4a4d605d789b520a2c71fdf08
Roman <blackwanted@yandex.ru>
2022-06-12 12:15:08 +0300

    Fix -- Start

---
+++ vue/component/ui/block.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="$style.block">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style module lang="scss">
+@import "../../vars";
+
+.block {
+  padding: 10px;
+  background-color: #191a1d;
+  border-radius: $radius-3;
+  display: flex;
+  flex-direction: column;
+}
+</style>
--- vue/component/ui/index.ts
+++ vue/component/ui/index.ts
@@ -2,5 +2,6 @@ import Button from "./button.vue";
 import Checkbox from "./checkbox.vue";
 import Toggle from "./toggle.vue";
 import Input from "./input.vue";
+import Block from "./block.vue";
 
-export { Button, Checkbox, Toggle, Input };
+export { Button, Checkbox, Toggle, Input, Block };
