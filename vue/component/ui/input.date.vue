d5d943427c6affa76bb265e11da45457dccf6f54
Roman <blackwanted@yandex.ru>
2023-01-11 21:33:06 +0300

    Fix -- Update

--- vue/component/ui/input.date.vue
+++ vue/component/ui/input.date.vue
@@ -2,29 +2,50 @@
   <div :class="$style.container">
     <!-- Calendar -->
     <div style="position: relative; display: flex">
-      <Button color="gray" @click="isShowCalendar = !isShowCalendar" style="border-radius: 4px; flex: 1">
-        {{ dayjs(modelValue).format('YYYY-MM-DD') }}
+      <Button
+        color="gray"
+        @click="isShowCalendar = !isShowCalendar"
+        style="border-radius: 4px; flex: 1"
+      >
+        {{ dayjs(modelValue).format("YYYY-MM-DD") }}
       </Button>
 
-      <Calendar v-if="isShowCalendar" @select="isShowCalendar = false" :class="$style.calendar" v-model="modelValue" />
+      <!-- <Calendar v-if="isShowCalendar" @select="isShowCalendar = false" :class="$style.calendar" v-model="modelValue" /> -->
     </div>
 
     <!-- Time -->
     <div style="position: relative; display: flex">
-      <Button color="gray" @click="isShowTime = !isShowTime" style="border-radius: 4px; flex: 1">
-        {{ dayjs(modelValue).format('HH:mm:ss') }}
+      <Button
+        color="gray"
+        @click="isShowTime = !isShowTime"
+        style="border-radius: 4px; flex: 1"
+      >
+        {{ dayjs(modelValue).format("HH:mm:ss") }}
       </Button>
       <div v-if="isShowTime" :class="$style.time">
-        <div style="display: grid; grid-template-columns: 1fr 1fr 1fr; gap: 10px; align-items: center">
+        <div
+          style="
+            display: grid;
+            grid-template-columns: 1fr 1fr 1fr;
+            gap: 10px;
+            align-items: center;
+          "
+        >
           <Button @click="offset(-1, 'hour')" color="gray">&lt;</Button>
-          <div>{{ dayjs(modelValue).format('HH') }}</div>
+          <div>{{ dayjs(modelValue).format("HH") }}</div>
           <Button @click="offset(1, 'hour')" color="gray">&gt;</Button>
         </div>
         <div
-          style="display: grid; grid-template-columns: 1fr 1fr 1fr; gap: 10px; align-items: center; margin-top: 10px"
+          style="
+            display: grid;
+            grid-template-columns: 1fr 1fr 1fr;
+            gap: 10px;
+            align-items: center;
+            margin-top: 10px;
+          "
         >
           <Button @click="offset(-1, 'minute')" color="gray">&lt;</Button>
-          <div>{{ dayjs(modelValue).format('mm') }}</div>
+          <div>{{ dayjs(modelValue).format("mm") }}</div>
           <Button @click="offset(1, 'minute')" color="gray">&gt;</Button>
         </div>
       </div>
@@ -33,7 +54,13 @@
     <Button
       @click="now()"
       color="gray"
-      style="padding: 2px; font-size: 12px; border-radius: 4px; width: 28px; height: 28px"
+      style="
+        padding: 2px;
+        font-size: 12px;
+        border-radius: 4px;
+        width: 28px;
+        height: 28px;
+      "
     >
       <IconStopwatch size="21" />
     </Button>
@@ -41,11 +68,11 @@
 </template>
 
 <script setup lang="ts">
-import dayjs from 'dayjs';
-import Button from '@/gam-lib-ui/vue/component/ui/button.vue';
-import { IconStopwatch } from '@/gam-lib-ui/vue/component/icon';
-import Calendar from '@/gam-lib-ui/vue/component/ui/calendar.vue';
-import { ref } from 'vue';
+import dayjs from "dayjs";
+import Button from "@/gam-lib-ui/vue/component/ui/button.vue";
+import { IconStopwatch } from "@/gam-lib-ui/vue/component/icon";
+import Calendar from "@/gam-lib-ui/vue/component/ui/calendar.vue";
+import { ref } from "vue";
 
 const props = defineProps<{
   modelValue: string;
@@ -53,22 +80,25 @@ const props = defineProps<{
 }>();
 
 // Vars
-const emit = defineEmits(['update:modelValue']);
+const emit = defineEmits(["update:modelValue"]);
 const isShowCalendar = ref(false);
 const isShowTime = ref(false);
 
 // Methods
-function offset(amount: number, type: 'hour' | 'minute') {
-  emit('update:modelValue', dayjs(props.modelValue).add(amount, type).format('YYYY-MM-DD HH:mm:ss.SSSZ'));
+function offset(amount: number, type: "hour" | "minute") {
+  emit(
+    "update:modelValue",
+    dayjs(props.modelValue).add(amount, type).format("YYYY-MM-DD HH:mm:ss.SSSZ")
+  );
 }
 
 function now() {
-  emit('update:modelValue', dayjs().format('YYYY-MM-DD HH:mm:ss.SSSZ'));
+  emit("update:modelValue", dayjs().format("YYYY-MM-DD HH:mm:ss.SSSZ"));
 }
 </script>
 
 <style module lang="scss">
-@import '../../vars';
+@import "../../vars";
 
 .container {
   width: 100%;
